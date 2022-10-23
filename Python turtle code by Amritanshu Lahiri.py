aef36048273b813897be7d211055b1a173348faa
Amritanshu Lahiri <116184733+Deadshot1611@users.noreply.github.com>
2022-10-23 09:53:26 +0530

    python turtle code #PR 1

---
+++ Python turtle code by Amritanshu Lahiri.py
@@ -0,0 +1,30 @@
+#Deadshot Quintus
+import turtle
+col=('lime' , 'red' , 'yellow' , 'blue' , 'magenta' , 'orange')
+t=turtle.Turtle()
+screen=turtle.Screen()
+screen.bgcolor('black')
+t.speed(40)
+for i in range(300):
+    t.color(col[i%6])
+    t.forward(i*1.5)
+    t.left(59)
+    t.width(3)
+    if i<26:
+        if i%3==0:
+            screen.bgcolor('white')
+        else:
+            screen.bgcolor('black')
+    elif i>=26 and i<74:
+        if i%2 ==0:
+            screen.bgcolor('white')
+        else:
+            screen.bgcolor('black')
+    else:
+        if i%3==0:
+            screen.bgcolor('white')
+        else:
+            screen.bgcolor('black')
+            
+
+#spiral created
