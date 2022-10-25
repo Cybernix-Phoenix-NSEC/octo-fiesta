45790e51cf1d11bd0efc520f2ed9113eee0f3d8e
GAUTAM BEHERA <116067064+dustinhoppper@users.noreply.github.com>
2022-10-25 13:40:51 +0530

    reversing of string

---
+++ reversing_string.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+void reverseStr(string& str)
+{
+    int n = str.length();
+ 
+    
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
+ 
+
+int main()
+{
+    string str;
+    cout<<"enter the string:";
+    cin>>str;
+    reverseStr(str);
+    cout << str;
+    return 0;
+}
