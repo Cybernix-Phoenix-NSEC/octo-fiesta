bb5f079c06fff0f2631e00f189f424f06c5b9ae2
maanavnair <114719385+maanavnair@users.noreply.github.com>
2022-10-26 12:46:34 +0530

    Add files via upload
    swaps two numbers

---
+++ swap.java
@@ -0,0 +1,11 @@
+public class swap{
+    public static void main(String[] args){
+        int a=5;
+        int b=7;
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("Swapped numbers are: ");
+        System.out.println("a="+a+" and b="+b);
+    }
+}
